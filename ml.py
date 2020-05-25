62e4a2103d8d990b445e726c8f13289aa39e18b4
yashmit28 <naksharma91.nks@gmail.com>
2020-05-25 19:35:21 +0530

    second

--- ml.py
+++ ml.py
@@ -1,95 +1,120 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from numpy import mean
-from numpy import std
-from sklearn.model_selection import KFold
-from keras.datasets import mnist
-from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.datasets import mnist
+from keras.utils import np_utils
+import keras
 
-# load train and test dataset
-def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = mnist.load_data()
-	# reshape dataset to have a single channel
-	trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
-	testX = testX.reshape((testX.shape[0], 28, 28, 1))
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
+#Load the mnist dataset 
+(X_train, y_train), (X_test, y_test)  = mnist.load_data()
 
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
+'''Changing the shape of input data from (60000,28,28) to (60000,28,28,1)
+as Keras takes input arrays of Dimensions 4'''
 
-# define cnn model
-def define_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Flatten())
-	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
-	model.add(Dense(10, activation='softmax'))
-	# compile model
-	opt = SGD(lr=0.01, momentum=0.9)
-	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-	return model
-
-# evaluate a model using k-fold cross-validation
-def evaluate_model(dataX, dataY, n_folds=5):
-	scores, histories = list(), list()
-	# prepare cross validation
-	kfold = KFold(n_folds, shuffle=True, random_state=1)
-	# enumerate splits
-	for train_ix, test_ix in kfold.split(dataX):
-		# define model
-		model = define_model()
-		# select rows for train and test
-		trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
-		# fit model
-		history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
-		# evaluate model
-		_, acc = model.evaluate(testX, testY, verbose=0)
-		print('> %.3f' % (acc * 100.0))
-		# stores scores
-		scores.append(acc)
-		histories.append(history)
-	return scores, histories
+X_train = X_train.reshape( X_train.shape[0] , X_train[0].shape[0] , X_train[1].shape[0] , 1 )
+X_test = X_test.reshape( X_test.shape[0] , X_test[0].shape[0] , X_test[1].shape[0] , 1 )
 
 
-# summarize model performance
-def summarize_performance(scores):
-	# print summary
-	print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+
+# store the shape of a single image 
+input_shape = ( X_train[0].shape[0] , X_train[1].shape[0], 1)
 
 
-# run the test harness for evaluating a model
-def run_test_harness():
-	# load dataset
-	trainX, trainY, testX, testY = load_dataset()
-	# prepare pixel data
-	trainX, testX = prep_pixels(trainX, testX)
-	# evaluate model
-	scores, histories = evaluate_model(trainX, trainY)
-	# summarize estimated performance
-	summarize_performance(scores)
 
-# entry point, run the test harness
-run_test_harness()
+# change our image type to float32 data type
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+
+
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+X_train /= 255
+X_test /= 255
+
+
+# Now we one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = X_train.shape[1] * X_train.shape[2]
+
+
+
+#Funtion for tweaking the parametres
+def CRP_layer(layers , filters , filterSize , poolSize , strideSize):
+  for i in range(layers):
+    model.add(Conv2D(filters, (filterSize, filterSize),
+                 padding = "same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size = (poolSize, poolSize), strides = (strideSize, strideSize)))
+  return
+
+#lets move to building our model
+
+model = Sequential()
+
+model.add(Conv2D(20, (5, 5),
+                 padding = "same", 
+                 input_shape = input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+#Calling Tweak file to get tweaking inputs
+import tweak as T
+X = T.run()
+
+print('additional CRP layers = {}'.format(X[0]))
+print('no. of filters = {}'.format(X[1]))
+print('size of filters = {}'.format(X[2]))
+print('size of pool = {}'.format(X[3]))
+print('size of stride = {}'.format(X[4]))
+
+#Calling the Additonal CRP layer adding function
+CRP_layer(X[0] , X[1] , X[2] , X[3] , X[4] )
+
+
+# Fully connected layers (w/ RELU)
+model.add(Flatten())
+model.add(Dense(500))
+model.add(Activation("relu"))
+
+# Softmax (for classification)
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
+           
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+
+
+# Training Parameters
+batch_size = 128
+epochs = 3
+
+history = model.fit(X_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(X_test, y_test),
+          shuffle=True)
+
+# Evaluate the performance of our trained model
+scores = model.evaluate(X_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
+oldaccuracy = open('/mlops/accuracy.txt','r')
+old = float(oldaccuracy.read())
+oldaccuracy.close()
+new  = scores[1]
+
+if new > old :
+  accuracyStored = open('/mlops/accuracy.txt','w')
+  accuracyStored.write(str(new))
+  accuracyStored.close()
+  model.save("ArpitFinalModel.h5")
+else :
+  print("No improverment , model is saved with previous accuracy of {}".format(old))
